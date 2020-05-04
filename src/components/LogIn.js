5adce26bfd3e839b4140774804da4afb95aeb2fa
Aleksandra000 <ao.dobrowolska@gmail.com>
2020-05-04 15:52:16 +0200

    email validation

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -3,22 +3,69 @@ import {Link} from "react-router-dom";
 
 
 class LogIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            wrongEmail: '',
+            wrongPassword: '',
+            verification: false
+        }
+    }
+    handleChange = e => {
+        const name = e.target.name;
+        this.setState({
+            [name]: e.target.value
+        });
+        e.target.value = "";
+    };
 
+    handleSubmit = e => {
+        e.preventDefault();
+        this.validate();
+        const isValid = this.validate();
+        if(isValid) {
+            this.setState({
+                wrongEmail: '',
+                wrongPassword: ''
+            });
+        }
+    };
+    validate = () => {
+        const patterns = {
+            email: /^([a-z\d\.-]+)@([a-z\d-]+).([a-z]{2,8})(\.[a-z]{2,8})?$/,
+        };
+        let wrongEmail = "";
+        let wrongPassword = "";
+        const { email, password } = this.state;
+        if (!patterns.email.test(email)) {
+            wrongEmail = "Podany email jest nieprawidłowy!";
+            this.setState({ wrongEmail })
+            return false;
+        }
+        if (password.length<6) {
+            wrongPassword = "Podane hasło jest za krótkie";
+            this.setState({ wrongPassword })
+            return false;
+        }
+        return true;
+    }
     render() {
         return (
             <div className="login">
                 <p className="login-title">Zaloguj się</p>
                 <div className="login-deco"></div>
-                <form >
+                <form onSubmit={this.handleSubmit}>
                     <div className="login-form">
                         <p className="labels">Email</p>
-                        <input  type="text" name="email" />
-
+                        <input onChange={this.handleChange} type="text" name="email" value={this.state.email} />
+                        <p className="error">{this.state.wrongEmail}</p>
                     </div>
                     <div className="login-form">
                         <p className="labels">Hasło</p>
-                        <input  type="password" name="password" />
-
+                        <input onChange={this.handleChange} type="password" name="password" value={this.state.password} />
+                        <p className="error">{this.state.wrongPassword}</p>
                     </div >
                     <Link className="login-button1 " to='/rejestracja'>Załóż konto</Link>
                     <input type="submit" className="login-button2" value="Zaloguj się"/>
@@ -26,7 +73,6 @@ class LogIn extends React.Component {
             </div>
         )
     }
-
 }
 
 export default LogIn;
