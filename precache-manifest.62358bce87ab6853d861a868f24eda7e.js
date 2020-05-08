81e9f5817357d7e4a75d1d72f4b74716bfd17e62
Aleksandra000 <ao.dobrowolska@gmail.com>
2020-05-08 18:30:36 +0200

    Updates

---
+++ precache-manifest.62358bce87ab6853d861a868f24eda7e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7e1fee4dd7c75127e4e1b429658a5e",
+    "url": "/OddamWDobreRece/index.html"
+  },
+  {
+    "revision": "bb9dffbc71d40043c43b",
+    "url": "/OddamWDobreRece/static/css/2.15abc31b.chunk.css"
+  },
+  {
+    "revision": "4ad00a58e80abddb9292",
+    "url": "/OddamWDobreRece/static/css/main.44c16b81.chunk.css"
+  },
+  {
+    "revision": "bb9dffbc71d40043c43b",
+    "url": "/OddamWDobreRece/static/js/2.9b724edb.chunk.js"
+  },
+  {
+    "revision": "4ad00a58e80abddb9292",
+    "url": "/OddamWDobreRece/static/js/main.79152887.chunk.js"
+  },
+  {
+    "revision": "8b9c16cbf5f0d2c2346c",
+    "url": "/OddamWDobreRece/static/js/runtime-main.b3ecd34c.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/OddamWDobreRece/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/OddamWDobreRece/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/OddamWDobreRece/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/OddamWDobreRece/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/OddamWDobreRece/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/OddamWDobreRece/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/OddamWDobreRece/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/OddamWDobreRece/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/OddamWDobreRece/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/OddamWDobreRece/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/OddamWDobreRece/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/OddamWDobreRece/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/OddamWDobreRece/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/OddamWDobreRece/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/OddamWDobreRece/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
